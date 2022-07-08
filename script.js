5187f84e484cf5a7d8188d293590fed4d32db6a0
Leandro Morillo <leandro.m9601@gmail.com>
2022-07-07 21:29:42 -0500

    add reset function

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 let billAmount = document.getElementById("bill-amount");
 let percentTip = document.getElementById("percentage-tip");
 let tipAmount = document.getElementById("tip-amount");
-let total = document.getElementById("total");
+let totalAmo = document.getElementById("total+");
+let totalDis = document.getElementById("total-");
 let calculate = document.getElementById("calculate");
+let reset = document.getElementById("reset");
+let myForm = document.getElementById("tCalculator");
 
 calculate.addEventListener("click", () => {
   billAmount = Number(billAmount.value);
   percentTip = Number(percentTip.value);
 
+  event.preventDefault();
+  calculateAmount();
+});
+
+reset.addEventListener("click", () => {
+    resetAmount();
+});
+
+function calculateAmount() {
   if (isNaN(billAmount) || billAmount <= 0 || billAmount === null) {
     alert("Please enter a valid Bill Amount");
   } else if (isNaN(percentTip) || percentTip <= 0 || percentTip === null) {
@@ -17,6 +29,13 @@ calculate.addEventListener("click", () => {
     tipAmount.value = "$" + calculatedTip;
 
     let calculatedTotal = billAmount + calculatedTip;
-    total.value = '$' + calculatedTotal;
+    totalAmo.value = "$" + calculatedTotal;
+
+    let calculatedTotal2 = billAmount - calculatedTip;
+    totalDis.value = "$" + calculatedTotal2;
   }
-});
+}
+
+function resetAmount() {
+    myForm.reset();
+}
